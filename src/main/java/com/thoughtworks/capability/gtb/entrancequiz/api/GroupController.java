47f2f17b32096538cf566c8f4da85f4acfd3baa7
Hourann <tdkgn@outlook.com>
2020-10-27 21:34:54 +0800

    [hourann] feat: add cors for group api

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/GroupController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/GroupController.java
@@ -4,6 +4,7 @@ package com.thoughtworks.capability.gtb.entrancequiz.api;
 import com.thoughtworks.capability.gtb.entrancequiz.domain.StudentGroup;
 import com.thoughtworks.capability.gtb.entrancequiz.service.GroupService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class GroupController {
     final GroupService groupService;
 
