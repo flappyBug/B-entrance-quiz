10ada821e609cc8b7038f9bf382a1f99c4de4b67
zhuyuqian <yuqian.zhu@thoughtworks.com>
2020-11-04 16:50:38 +0800

    [zhuyuqian]GTB comments

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
@@ -11,3 +11,10 @@ public class GtbEntranceQuizApplication {
 	}
 
 }
+
+// TODO GTB-完成度: * 新增学员的接口没有校验姓名为空的情况
+// TODO GTB-完成度: * 查询学员列表的接口返回了所有学员，与需求不符合
+// TODO GTB-测试: * 包含了对Controller的测试，但是没有测到什么，没有遵循测试最佳实践
+// TODO GTB-知识点: * Spring MVC相关的知识点掌握的不错
+// TODO GTB-工程实践: * 项目划分合理，使用了分层架构
+// TODO GTB-工程实践: * git commit message表意，遵循了小步提交
--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentController.java
@@ -7,16 +7,19 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// TODO GTB-知识点: - @CrossOrigin避免使用通配符，应指定具体的域名
 @RestController
 @CrossOrigin(origins = "*")
 public class StudentController {
 
+    // TODO GTB-工程实践: - 建议使用private，遵循最小访问原则
     final StudentService studentService;
 
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
+    // TODO GTB-知识点: - @RestController和ResponseEntity重复
     @GetMapping("/students")
     ResponseEntity<List<Student>> getStudents() {
         return ResponseEntity.ok(studentService.getStudents());
@@ -25,6 +28,7 @@ public class StudentController {
     @PostMapping("/students")
     ResponseEntity<Void> addStudent(@RequestBody Student student) {
         studentService.addStudent(student);
+        // TODO GTB-工程实践: - 及时清理无用代码
         //noinspection ConstantConditions
         return ResponseEntity.created(null).build();
     }
